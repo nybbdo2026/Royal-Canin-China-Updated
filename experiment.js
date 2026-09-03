a6b01114163c4e547b566dd5f364b21e346ae36c
nybbdo2026 <kk17104408@gmail.com>
2026-09-04 00:48:20 +0530

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -973,7 +973,9 @@ function generateMultiplePretestTrials(attributes, images) {
 
   attributes.forEach(attr => {
     // Identify the correct image for this attribute
-    const correctImage = images.find(img =>   Array.isArray(img.correct) &&   img.correct.includes(attr) );
+    const correctImage = images.find(
+  img => Array.isArray(img.correct) && img.correct.includes(attr)
+);
     const otherImages = images.filter(img => img !== correctImage);
     
     // Choose 3 random distractors
